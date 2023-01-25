fd4cb0d4f66474d6b6c1924b1af97b3d64c9bcf0
Eugene Chevski <eugenechevski@proton.me>
2023-01-25 15:57:11 -0500

    create the AfterCreation component

--- src/components/landing-page/AfterCreation.tsx
+++ src/components/landing-page/AfterCreation.tsx
@@ -1,7 +1,10 @@
 const AfterCreation = () => {
     return (
-        <div>
-            After Creation
+        <div className="p-12 w-3/4 sm:w-1/4 rounded-xl bg-base-100">
+            <div className="w-full h-full flex flex-col items-center justify-end gap-12">
+                <div className="font-bold text-center mt-6">The account has been created.</div>
+                <div className="btn-primary rounded-full w-3/4 text-center">Procceed</div>
+            </div>
         </div>
     )
 };
--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -5,7 +5,7 @@ import HelloPage from './HelloPage';
 const LandingPage = () => {
     return (
         <div className='flex items-center justify-center h-full w-full'>
-            <CreateAccountForm></CreateAccountForm>
+            <AfterCreation></AfterCreation>
         </div>
     )
 };
