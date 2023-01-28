ac77dc18711525c955ab95b83c9b876640796848
Eugene Chevski <eugenechevski@proton.me>
2023-01-27 20:02:36 -0500

    adds the support for text shadows to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: [
     "./src/*.{js,jsx,ts,tsx}",
@@ -7,12 +9,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
       fontFamily: {
         'Lato': ['Lato', 'sans-serif'], 
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    plugin(function ( { matchUtilities, theme} ) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
   daisyui: {
     themes: ['garden'],
   },
