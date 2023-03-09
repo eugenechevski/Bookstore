e3b7eac682d7033531877207415f6747809b4c2b
Eugene Chevski <eugenechevski@proton.me>
2023-03-08 20:54:32 -0500

    adds the User factory

---
+++ src/scripts/User.ts
@@ -0,0 +1,80 @@
+export default function User(firstName: string, 
+                             lastName: string, 
+                             password: string, 
+                             email: string, 
+                             wishlist: Set<Book>, 
+                             cart: Set<Book>): User {
+    
+    function getFirstName(): string {
+        return firstName;
+    }
+
+    function getLastName(): string {
+        return lastName;
+    }
+
+    function getPassword(): string {
+        return password;
+    }
+
+    function getEmail(): string {
+        return email;
+    }
+
+    function getWishlist(): Set<Book> {
+        return wishlist;
+    }
+
+    function getCart(): Set<Book> {
+        return cart;
+    }
+
+    function setFirstName(first: string) {
+        firstName = first;
+    }
+
+    function setLastName(last: string) {
+        lastName = last;
+    }
+
+    function setEmail(newEmail: string) {
+        email = newEmail;
+    }
+
+    function setPassword(newPassword: string) {
+        password = newPassword;   
+    }
+
+    function addToWishList(book: Book) {
+        wishlist.add(book);
+    }
+
+    function addToCart(book: Book) {
+        cart.add(book);
+    }
+
+    function removeFromCart(book: Book) {
+        cart.delete(book);
+    }
+
+    function removeFromWishlist(book: Book) {
+        wishlist.delete(book);
+    }
+
+    return {
+        getFirstName,
+        getLastName,
+        getPassword,
+        getEmail,
+        getWishlist,
+        getCart,
+        setFirstName,
+        setLastName,
+        setEmail,
+        setPassword,
+        addToWishList,
+        addToCart,
+        removeFromCart,
+        removeFromWishlist,
+    }
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -9,3 +9,20 @@ declare type Category = {
  getName: () => string,
  getBooks: () => Book[],
 };
+
+declare type User = {
+  getFirstName: () => string,
+  getLastName: () => string,
+  getPassword: () => string,
+  getEmail: () => string,
+  getWishlist: () => Set<Book>,
+  getCart: () => Set<Book>,
+  setFirstName: (first: string) => void,
+  setLastName: (last: string) => void,
+  setPassword: (newPassword: string) => void,
+  setEmail: (newEmail: string) => void,
+  addToWishList: (book: Book) => void,
+  addToCart: (book: Book) => void,
+  removeFromCart: (book: Book) => void,
+  removeFromWishlist: (book: Book) => void,
+}
