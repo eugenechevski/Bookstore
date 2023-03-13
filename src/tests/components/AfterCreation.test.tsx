6dab921160d202fa4ba2e6a74289df1066534461
Eugene Chevski <eugenechevski@proton.me>
2023-03-13 19:14:00 -0400

    tests the AfterCreation component

---
+++ src/tests/components/AfterCreation.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import AfterCreation from 'components/forms/AfterCreation';
+
+describe('AfterCreation component', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <AfterCreation></AfterCreation>
+      </MemoryRouter>
+    );
+  });
+
+  it("it displays a message after an account has been created", () => {
+    expect(screen.getByText(/The account has been created./i)).toBeInTheDocument();
+  });
+
+  
+});
