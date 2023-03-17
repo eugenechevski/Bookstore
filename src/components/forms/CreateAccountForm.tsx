601e5b992845d98a1f575161995e478e7d9acc6a
Eugene Chevski <eugenechevski@proton.me>
2023-03-17 16:10:31 -0400

    adds the form validation for the CreateAccountForm

--- src/components/forms/CreateAccountForm.tsx
+++ src/components/forms/CreateAccountForm.tsx
@@ -1,7 +1,72 @@
 import Page from "components/general/Page";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import useForm from "utils/useForm";
+import { useNavigate } from "react-router-dom";
+
+const validate = (values: {
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string,
+  confirmPassword: string,
+}) => {
+   // Define a function that takes the state values and returns an error object
+   let errors: {
+     firstName?: string,
+     lastName?: string,
+     email?: string,
+     password?: string,
+     confirmPassword?: string,
+   } = {};
+
+   const firstNameRegex = /^[a-zA-Z]{3,}$/;
+
+   if (!firstNameRegex.test(values.firstName)) {
+     errors.firstName = "First name must be at least 3 characters";
+   } 
+
+   const lastNameRegex = firstNameRegex;
+   
+   if (!lastNameRegex.test(values.lastName)) {
+     errors.lastName = "Last name must be at least 3 characters";
+   }
+   
+   const emailRegex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+   if (!emailRegex.test(values.email)) {
+     errors.email = "Invalid email address";
+   }
+
+   const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$^#!%*()+-?&])[a-zA-Z\d@$()+-^#!%*?&]{8,}/;
+   
+   if (!passwordRegex.test(values.password)) {
+     errors.password = "Password must be at least 8 characters, include at least one uppercase letter, one lowercase letter, one number and one special character";
+   }
+
+   if (values.password !== '' && values.password !== values.confirmPassword) {
+     errors.confirmPassword = "Passwords do not match";
+   }
+
+   return errors; // Return the error object
+};
+
 
 const CreateAccountForm = () => {
+    const navigate = useNavigate();
+    const doStuffOnceValid = () => {
+       navigate('/after-creation');
+    };
+
+    const {handleSubmit, handleChange, values, errors} = useForm({ 
+       firstName: '',
+       lastName: '',
+       email: '',
+       password: '',
+       confirmPassword: ''
+     }, 
+     validate,
+     doStuffOnceValid);
+
     return (
         <Page content={
             <div className="p-6 
@@ -9,43 +74,109 @@ const CreateAccountForm = () => {
                             rounded-xl 
                             bg-base-100 
                             sm:w-1/4">
-                <div className="form-control 
-                                text-sm 
-                                w-full 
-                                h-full 
-                                justify-center">
-                    <label className="label 
-                                    font-bold">First Name</label>
-                    <input type="text" className="input-sm 
-                                                rounded-lg 
-                                                input-bordered" />
-                    <label className="label 
-                                    font-bold">Last Name</label>
-                    <input type="text" className="input-sm 
-                                                rounded-lg 
-                                                input-bordered" />
-                    <label className="label 
-                                    font-bold">E-mail</label>
-                    <input type="text" className="input-sm 
-                                                rounded-lg 
-                                                input-bordered" />
-                    <label className="label 
-                                    font-bold">Password</label>
-                    <input type="text" className="input-sm 
-                                                rounded-lg 
-                                                input-bordered" />
-                    <label className="label 
-                                    font-bold">Repeat password</label>
-                    <input type="text" className="input-sm 
-                                                rounded-lg 
-                                                input-bordered" />
-                    <Link to={"/after-creation"} className="btn-primary 
-                                                            rounded-full 
-                                                            w-1/2 
-                                                            self-center 
-                                                            mt-12
-                                                            text-center">Create</Link>
-                </div>
+                <form onSubmit={handleSubmit}
+                      className="form-control
+                                 text-sm
+                                 w-full
+                                 h-full
+                                 justify-center">
+                    {/** First name */}
+                    <div className="flex
+                                    flex-col">
+                      <label htmlFor="firstName"
+                             className="label
+                                        font-bold">First Name</label>
+                      <input onChange={handleChange}
+                             id="firstName"
+                             type="text"
+                             name="firstName"
+                             value={values.firstName}
+                             className="input-sm
+                                        rounded-lg
+                                        input-bordered"
+                      />
+                      { errors.firstName && <p>{ errors.firstName }</p> }
+                    </div>
+                     
+                     {/** Last name */}
+                    <div className="flex
+                                    flex-col">
+                      <label htmlFor="lastName"
+                             className="label
+                                           font-bold">Last Name</label>
+                      <input onChange={handleChange}
+                             name="lastName"
+                             value={values.lastName}
+                             id="lastName"
+                             type="text"
+                             className="input-sm
+                                        rounded-lg
+                                        input-bordered"
+                      />
+                      { errors.lastName && <p>{ errors.lastName }</p> } 
+                    </div>
+
+                    {/** Email */}
+                    <div className="flex
+                                    flex-col">
+                      <label htmlFor="email"
+                             className="label 
+                                        font-bold">E-mail</label>
+                      <input onChange={handleChange}
+                             name="email"
+                             value={values.email}
+                             id="email"
+                             type="text"
+                             className="input-sm
+                                        rounded-lg
+                                        input-bordered" 
+                       />            
+                       { errors.email && <p>{ errors.email }</p> } 
+                    </div>
+
+                    {/** Password */}
+                    <div className="flex
+                                    flex-col">
+                      <label htmlFor="password"
+                             className="label
+                                        font-bold">Password</label>
+                      <input onChange={handleChange}
+                             name="password"
+                             value={values.password}
+                             id="password"
+                             type="password" 
+                             className="input-sm
+                                        rounded-lg
+                                        input-bordered" />
+                      { errors.password && <p>{ errors.password }</p> }
+                    </div>
+                    
+                    {/** Confirm password */}
+                    <div className="flex
+                                    flex-col">
+                      <label htmlFor="confirmPassword"
+                             className="label
+                                        font-bold">Confirm Password</label>
+                      <input onChange={handleChange}
+                             name="confirmPassword"
+                             value={values.confirmPassword}
+                             id="confirmPassword"
+                             type="password" 
+                             className="input-sm
+                                        rounded-lg
+                                        input-bordered" 
+                      />
+                      {errors.confirmPassword && <p>{ errors.confirmPassword }</p>}
+                    </div>
+
+                    {/** Submit button */}
+                    <button type="submit" className="btn-primary 
+                                                     rounded-full 
+                                                     w-1/2 
+                                                     self-center 
+                                                     mt-12
+                                                     text-center">Create</button>
+                </form>
             </div>
         } blank={true}></Page>
     );
---
+++ src/utils/useForm.ts
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react';
+
+const useForm = (initialState: any, validate: (values: any) => any, doStuffOnceValid: () => any): {
+  values: any,
+  errors: any,
+  handleChange: (e: any) => void,
+  handleSubmit: (e: any) => void
+} => {
+    // Declare state variables for values and errors
+    const [values, setValues] = useState(initialState);
+    const [errors, setErrors] = useState({});
+    
+    // Define a function that handles the input change event
+    const handleChange = (event) => {
+      // Get the name and value of the input element
+      const { name, value } = event.target;
+      // Update the state values with the new input value
+      setValues({
+        ...values,
+        [name]: value,
+      });
+    };
+    
+    // Define a function that handles the form submit event
+    const handleSubmit = (event) => {
+      // Prevent default browser behavior
+      event.preventDefault();
+      // Validate the state values using the validate function
+      const validationErrors = validate(values);
+      // Set the state errors with the validation errors
+      setErrors(validationErrors);
+
+      // If validation was successful, do stuff
+      if (Object.keys(errors).length === 0) {
+        console.log('All good!');
+        doStuffOnceValid();
+      }
+    };
+    
+    return { values, errors, handleChange, handleSubmit }; // Return an object with state and handlers
+};
+
+export default useForm;
