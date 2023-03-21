553e21e3a2ef14f51fd550fe9f920bfffa1b2a53
Eugene Chevski <eugenechevski@proton.me>
2023-03-21 15:14:45 -0400

    tests the AfterSignIn component

---
+++ src/tests/components/AfterSingIn.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import AfterSignIn from 'components/forms/AfterSignIn';
+
+describe('AfterSignIn component', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <AfterSignIn></AfterSignIn>
+      </MemoryRouter>
+    );
+  });
+
+  it("it displays a message after an account has been created", () => {
+    expect(screen.getByText(/Signed-in successfully./i)).toBeInTheDocument();
+  });
+
+    
+});
