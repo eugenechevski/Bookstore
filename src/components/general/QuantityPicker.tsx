5836d7794d4ba92fe5ad7400888d62f0ba81b351
Eugene Chevski <eugenechevski@proton.me>
2023-04-29 20:43:45 -0400

    Creates the QuantityPicker component

---
+++ src/components/general/QuantityPicker.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import IconButton from './IconButton';
+
+const QuantityPicker = ({ value, onChange }: { value: number, onChange: (value: number) => void }) => {
+  const [quantity, setQuantity] = useState(value);
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+    onChange(quantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantity > 0) {
+      const newQuantity = quantity - 1;
+      setQuantity(newQuantity);
+      onChange(newQuantity);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <IconButton
+        iconName={'arrow-up'}
+        classes="btn btn-ghost btn-sm rounded-r"
+        onClickListener={handleIncrement}
+      />
+      <span className="flex-1 text-center">{quantity}</span>
+      <IconButton
+        iconName={'arrow-down'}
+        classes="btn btn-ghost btn-sm rounded-l"
+        onClickListener={handleDecrement}
+      />
+    </div>
+  );
+};
+
+export default QuantityPicker;
