74fe74cddf57a08db8d0e02dff9e9a72b672f816
Eugene Chevski <eugenechevski@proton.me>
2023-05-01 12:43:01 -0400

    Implements the checking-out process

--- src/components/forms/CheckoutForm.tsx
+++ src/components/forms/CheckoutForm.tsx
@@ -1,22 +1,55 @@
 import Page from "components/general/Page";
-import CheckoutBook from "./CheckoutBook";
-import uniqid from "uniqid";
-import { Link } from "react-router-dom";
 import { DataContext } from "components/App";
+import CheckoutBook from "./CheckoutBook";
+import TextButton from "components/general/TextButton";
+
+import generateAddToCartLink from "utils/cartLinkGenerator";
+
+import uniqid from "uniqid";
+import { useNavigate } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 
 const CheckoutForm = ({ testUser }: { testUser?: IUser }) => {
   const { user, userCart, setUserCart } = useContext(DataContext);
+  const navigate = useNavigate();
+
   const stateUser = testUser !== undefined ? testUser : user;
   const [cartItems, setCartItems] = useState<IBook[]>(
     testUser !== undefined ? (stateUser as IUser).getCart() : userCart
   );
+  const [addToCartLink, setAddToCartLink] = useState<string>("");
 
+  // Load cart items
   useEffect(() => {
     if (userCart !== undefined) {
       setCartItems(userCart);
     }
   }, [userCart]);
+
+  // Create add to cart link
+  useEffect(() => {
+    if (cartItems.length > 0 && user && Object.keys(user).length > 0) {
+      setAddToCartLink(generateAddToCartLink(cartItems, user as IUser));
+    }
+  }, [cartItems, user])
+
+  const openInNewTab = (url: string) => {
+    const newTab = window.open(url, '_blank');
+    if (newTab) {
+      newTab.focus();
+    }
+  }
+
+  const handleCheckout = () => {
+    if (addToCartLink.length > 0) {
+      openInNewTab(addToCartLink);
+      navigate("/after-checkout");
+      (user as IUser).emptyCart();
+      setUserCart((user as IUser).getCart());
+    } else {
+      navigate("/home");
+    }
+  };
 
   return (
     <Page
@@ -36,16 +69,16 @@ const CheckoutForm = ({ testUser }: { testUser?: IUser }) => {
                                 items-center
                                 justify-center"
           >
-            {/* Items */}
             <div
               className="flex
-                         flex-col
-                         gap-6
-                         scrollbar
-                         max-h-[444px]
-                         sm:max-h-[540px]
-                         overflow-y-scroll"
-            >
+              flex-col
+              gap-6
+              scrollbar
+              max-h-[444px]
+              sm:max-h-[540px]
+              overflow-y-scroll"
+              >
+              {/* Items */}
               {cartItems?.map((book: IBook) => (
                 <CheckoutBook
                   key={uniqid()}
@@ -61,18 +94,18 @@ const CheckoutForm = ({ testUser }: { testUser?: IUser }) => {
                 />
               ))}
             </div>
+
             {/* Checkout button */}
-            <Link
-              to={cartItems.length > 0 ? "/after-checkout" : "/home"}
-              className="btn-primary 
-                                                            rounded-full 
-                                                            w-1/2 
-                                                            self-center 
-                                                            mt-12
-                                                            text-center"
-            >
-              Checkout
-            </Link>
+            <TextButton
+              textContent={"Checkout"}
+              onClickListener={handleCheckout}
+              classes="btn-primary 
+                       rounded-full 
+                       w-1/2 
+                       self-center 
+                       mt-12
+                       text-center"
+            />
           </div>
         </div>
       }
---
+++ src/utils/cartLinkGenerator.ts
@@ -0,0 +1,31 @@
+function extractASINFromAmazonURL(url: string): string | null {
+  const regex = /\/([A-Z0-9]{10})(?:[/?]|$)/i;
+  const match = url.match(regex);
+  if (match && match.length >= 2) {
+    return match[1];
+  }
+  return null;
+}
+
+function generateAddToCartLink(books: IBook[], user: IUser): string {
+  const asins: string[] = [];
+  let link = "https://www.amazon.com/gp/aws/cart/add.html?";
+
+  books.forEach((book) => {
+    book.getBuyLinks().forEach(({name, url}) => {
+        if (name === "Amazon") {
+            const asin = extractASINFromAmazonURL(url);
+            if (asin) {
+              asins.push(asin);
+              const formattedTitle = book.getFormattedTitle();
+              const quantity = user.getQuantity(formattedTitle);
+              link += `&ASIN.${asins.length}=${asin}&Quantity.${asins.length}=${quantity}`;
+            }
+        }
+    })
+  });
+
+  return link;
+}
+
+export default generateAddToCartLink;
